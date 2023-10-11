9dc0cdaad0951b8735fd4d4173904c0ced6600cd
Sylvia T <sylviarosemarie@gmail.com>
2023-10-11 11:35:07 -0500

    Added getter and setter functions decorators to all animals + chip_number attribute. Ensured it was protected as recommended.

--- index.py
+++ index.py
@@ -4,77 +4,78 @@ from slithering import Alligator, Gecko, Hognose, Skink, Snork
 from swimming import Axolotl, Catfish, Goldfish, Octopus, Shark
 from walking import Cougar, Donkey, Goose, Horse, Llama
 
-alfred = Alligator("Alfred", "American alligator", "afternoon", "fish")
+alfred = Alligator("Alfred", "American alligator", "afternoon", "fish", 123456)
+
 print(alfred)
 print(f'{alfred.name} the {alfred.species} is available to pet during the {alfred.shift} shift.')
 print(alfred.feed())
 
-martin = Gecko("Martin", "Crested gecko", "morning", "crickets")
+martin = Gecko("Martin", "Crested gecko", "morning", "crickets", 234567)
 print(martin)
 print(f'{martin.name} the {martin.species} is available to pet during the {martin.shift} shift.')
 print(martin.feed())
 
-baby_cakes = Hognose("Baby Cakes", "Southern Hognose snake", "midday", "a feeder mouse")
+baby_cakes = Hognose("Baby Cakes", "Southern Hognose snake", "midday", "a feeder mouse", 345678)
 print(baby_cakes)
 print(f'{baby_cakes.name} the {baby_cakes.species} is available to pet during the {baby_cakes.shift} shift.')
 print(baby_cakes.feed())
 
-stink = Skink("Stink", "Blue-tongued skink", "morning", "leafy greens")
+stink = Skink("Stink", "Blue-tongued skink", "morning", "leafy greens", 456789)
 print(stink)
 print(f'{stink.name} the {stink.species} is available to pet during the {stink.shift} shift.')
 print(stink.feed())
 
-squish_pls = Snork("Squish", "Ball python", "afternoon", "a rodent")
+squish_pls = Snork("Squish", "Ball python", "afternoon", "a rodent", 567890)
 print(squish_pls)
 print(f'{squish_pls.name} the {squish_pls.species} is available to pet during the {squish_pls.shift} shift.')
 print(squish_pls.feed())
 
-marisol = Axolotl("Marisol","Mosaic axolotl", "morning", "nightcrawlers")
+marisol = Axolotl("Marisol","Mosaic axolotl", "morning", "nightcrawlers", 678901)
 print(marisol)
 print(f'{marisol.name} the {marisol.species} is available to view during the {marisol.shift} shift.')
 print(marisol.feed())
 
-icky_thump = Catfish("Icky Thump", "Armored catfish", "midday", "zucchini and squash")
+icky_thump = Catfish("Icky Thump", "Armored catfish", "midday", "zucchini and squash", 789012)
 print(icky_thump)
 print(f'{icky_thump.name} the {icky_thump.species} is available to pet during the {icky_thump.shift} shift.')
 print(icky_thump.feed())
 
-goldilocks = Goldfish("Goldilocks","Ryukin goldfish", "midday", "thawed blood worms")
+goldilocks = Goldfish("Goldilocks","Ryukin goldfish", "midday", "thawed blood worms", 890123)
 print(goldilocks)
 print(f'{goldilocks.name} the {goldilocks.species} is available to pet during the {goldilocks.shift} shift.')
 print(goldilocks.feed())
 
-ronald = Octopus("Ronald","Umbrella octopus", "afternoon", "frozen razor shell clams")
+ronald = Octopus("Ronald","Umbrella octopus", "afternoon", "frozen razor shell clams", 901234)
 print(ronald)
 print(f'{ronald.name} the {ronald.species} is available to pet during the {ronald.shift} shift.')
 print(ronald.feed())
 
-bruce = Shark("Bruce", "Lemon shark", "morning", "crabs and lobster")
+bruce = Shark("Bruce", "Lemon shark", "morning", "crabs and lobster", 912345)
 print(bruce)
 print(f'{bruce.name} the {bruce.species} is available to pet during the {bruce.shift} shift.')
 print(bruce.feed())
 
-madam_milf = Cougar("Madam","North American cougar", "midday", "venison")
+madam_milf = Cougar("Madam","North American cougar", "midday", "venison", 123459)
 print(madam_milf)
 print(f'{madam_milf.name} the {madam_milf.species} is available to pet during the {madam_milf.shift} shift.')
 print(madam_milf.feed())
 
-arsenio = Donkey("Arsenio","regular ol' donkey", "morning", "straw")
+arsenio = Donkey("Arsenio","regular ol' donkey", "morning", "straw", 234569)
 print(arsenio)
 print(f'{arsenio.name} the {arsenio.species} is available to pet during the {arsenio.shift} shift.')
 print(arsenio.feed())
 
-sir_honk = Goose("Sir Honk", "common goose", "afternoon", "orchard and timothy hay")
+sir_honk = Goose("Sir Honk", "common goose", "afternoon", "orchard and timothy hay", 345679)
 print(sir_honk)
 print(f'{sir_honk.name} the {sir_honk.species} is available to pet during the {sir_honk.shift} shift.')
 print(sir_honk.feed())
 
-fusaichi_pegasus = Horse("Fusaichi Pegasus","American Thoroughbred horse", "morning", "timothy and oat hay")
+fusaichi_pegasus = Horse("Fusaichi Pegasus","American Thoroughbred horse", "morning", "timothy and oat hay", 456781)
 print(fusaichi_pegasus)
 print(f'{fusaichi_pegasus.name} the {fusaichi_pegasus.species} is available to pet during the {fusaichi_pegasus.shift} shift.')
 print(fusaichi_pegasus.feed())
 
-miss_fuzz = Llama("Miss Fuzz","domestic llama", "midday", "clover and blackberries")
+miss_fuzz = Llama("Miss Fuzz","domestic llama", "midday", "clover and blackberries", 567819)
 # attr = vars(miss_fuzz)
 # print(attr)
 print(miss_fuzz)
@@ -89,7 +90,7 @@ fluffy_funhouse.add_animal(fusaichi_pegasus)
 fluffy_funhouse.add_animal(miss_fuzz)
 print(f"{fluffy_funhouse.attraction_name} is where you'll find the {fluffy_funhouse.description} animals:")
 for animal in fluffy_funhouse.animals:
-    print(f'- {animal.name} the {animal.species}')
+    print(f'- {animal.name} the {animal.species} - {animal.chip_number}')
 
 viper_village = SnakePit("Viper Village")
 viper_village.add_animal(alfred)
@@ -99,7 +100,7 @@ viper_village.add_animal(stink)
 viper_village.add_animal(squish_pls)
 print(f"{viper_village.attraction_name} is where {viper_village.description} live:")
 for animal in viper_village.animals:
-    print(f'- {animal.name} the {animal.species}')
+    print(f'- {animal.name} the {animal.species} - {animal.chip_number}')
 
 atchafalaya_swamp = WetLands("Atchafalaya Swamp")
 atchafalaya_swamp.add_animal(marisol)
@@ -109,4 +110,5 @@ atchafalaya_swamp.add_animal(ronald)
 atchafalaya_swamp.add_animal(bruce)
 print(f"{atchafalaya_swamp.attraction_name} is where you'll find {atchafalaya_swamp.description} animals:")
 for animal in atchafalaya_swamp.animals:
-    print(f'- {animal.name} the {animal.species}')
+    print(f'- {animal.name} the {animal.species} - {animal.chip_number}')
+
--- walking/Donkey.py
+++ walking/Donkey.py
@@ -3,7 +3,7 @@ from datetime import date
 
 class Donkey:
     """Class representing an animal"""
-    def __init__(self, name, species, shift, food):
+    def __init__(self, name, species, shift, food, chip):
         # Establish the properties of each animal
         # with a default value
         self.name = name
@@ -12,6 +12,7 @@ class Donkey:
         self.walking = True
         self.shift = shift
         self.food = food
+        self.chip_number = chip
 
     def __str__(self):
         return f"{self.name} is a/n {self.species}"
@@ -19,4 +20,25 @@ class Donkey:
     def feed(self):
         """Method to allow for the feeding of animals"""
         return f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}'
+
+    @property
+    def chip_number(self):
+        return self.__chip_number
     
+    @chip_number.setter
+    def chip_number(self, number):
+        pass
+
+    @property # The getter
+    def chip_number(self):
+        try:
+            return self.__chip_number # Note there are 2 underscores here
+        except AttributeError:
+            return 0
+
+    @chip_number.setter # The setter
+    def chip_number(self, new_chip_number):
+        if type(new_chip_number) is int:
+            self.__chip_number = new_chip_number
+        else:
+            raise TypeError('Please provide an integer value for the chip number')    
