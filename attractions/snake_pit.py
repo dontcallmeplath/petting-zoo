cb105675535124e2ba14451d4fb7810ef28417fe
Sylvia T <sylviarosemarie@gmail.com>
2023-10-11 23:09:52 -0500

    refactored attractions to all have type checking methods

--- attractions/snake_pit.py
+++ attractions/snake_pit.py
@@ -1,5 +1,6 @@
 """Module to create instance of attraction re snake pit"""
 from .attraction import Attraction
+from movements import Slithering
 
 class SnakePit(Attraction):
     """Class creating instances of snake pits"""
@@ -14,4 +15,20 @@ class SnakePit(Attraction):
     def last_critter_added(self):
         """Method for returning last animal in list"""
         return f'{self.animals[-1].name} the {self.animals[-1].species}'
+
+    def add_animal_pythonic(self, animal):
+        try:
+            if animal.slithering:
+                self.animals.append(animal)
+                print(f"{animal} now lives in {self.attraction_name}")
+        except AttributeError as ex:
+            print(f'{animal} doesn\'t like to be petted, so please do not put it in the {self.attraction_name} attraction.')
+
+    # Number 2: Actual typing check
+    def add_animal_type_check(self, animal):
+        if isinstance(animal, Slithering):
+            self.animals.append(animal)
+            print(f"{animal} now lives in {self.attraction_name}")
+        else:
+            print(f'{animal} doesn\'t like to be petted, so please do not try to put it in the {self.attraction_name} attraction.')    
     
--- attractions/wetlands.py
+++ attractions/wetlands.py
@@ -1,5 +1,6 @@
 """Module to create instance of attraction re wetlands"""
 from .attraction import Attraction
+from movements import Swimming
 
 class WetLands(Attraction):
     """Class creating instances of wetlands"""
@@ -14,4 +15,20 @@ class WetLands(Attraction):
     def last_critter_added(self):
         """Method for returning last animal in list"""
         return f'{self.animals[-1].name} the {self.animals[-1].species}'
+    
+    def add_animal_pythonic(self, animal):
+        try:
+            if animal.swimming:
+                self.animals.append(animal)
+                print(f"{animal} now lives in {self.attraction_name}")
+        except AttributeError as ex:
+            print(f'{animal} doesn\'t like to be petted, so please do not put it in the {self.attraction_name} attraction.')
+
+    # Number 2: Actual typing check
+    def add_animal_type_check(self, animal):
+        if isinstance(animal, Swimming):
+            self.animals.append(animal)
+            print(f"{animal} now lives in {self.attraction_name}")
+        else:
+            print(f'{animal} doesn\'t like to be petted, so please do not try to put it in the {self.attraction_name} attraction.')
     
